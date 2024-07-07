1188e5a24e96f71ee4fc5786a19d8e024402c92b
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-07-07 18:13:33 +0600

    Factory Created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,34 +15,34 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(5)->create();
-
+        Listing::factory(5)->create();
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Listing::create(
-            [
-                'title'=>'Fullstack Web Developer',
-                'tags'=>'web, laravel',
-                'company'=>'InfoTech',
-                'location'=>'Dhaka',
-                'email'=>'example1@email.com',
-                'website'=>'www.infotech.com',
-                'description'=> 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, eligendi?'
+        // Listing::create(
+        //     [
+        //         'title'=>'Fullstack Web Developer',
+        //         'tags'=>'web, laravel',
+        //         'company'=>'InfoTech',
+        //         'location'=>'Dhaka',
+        //         'email'=>'example1@email.com',
+        //         'website'=>'www.infotech.com',
+        //         'description'=> 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, eligendi?'
 
-            ]
-        );
-        Listing::create(
-            [
-                'title'=>'Android Developer',
-                'tags'=>'android, app',
-                'company'=>'TechTune',
-                'location'=>'Dhaka',
-                'email'=>'example2@email.com',
-                'website'=>'www.techtune.com',
-                'description'=> 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, eligendi?'
+        //     ]
+        // );
+        // Listing::create(
+        //     [
+        //         'title'=>'Android Developer',
+        //         'tags'=>'android, app',
+        //         'company'=>'TechTune',
+        //         'location'=>'Dhaka',
+        //         'email'=>'example2@email.com',
+        //         'website'=>'www.techtune.com',
+        //         'description'=> 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, eligendi?'
 
-            ]
-        );
+        //     ]
+        // );
     }
 }

e9f322de7fed9f90a0b5c06dde5eba6f02cbe98b
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-07-11 00:34:03 +0600

    Layout and Section Created

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,14 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
 
-@if(!empty($listings))
-    @foreach($listings as $listing)
-    <h2><a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a></h2>
-    <p>{{$listing['description']}}</p>
-    @endforeach
-@else 
-    <p>No listings available</p>
-@endif
+@section('content')
+    <h1>{{$heading}}</h1>
+
+    @if(!empty($listings))
+        @foreach($listings as $listing)
+        <h2><a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a></h2>
+        <p>{{$listing['description']}}</p>
+        @endforeach
+    @else 
+        <p>No listings available</p>
+    @endif
+@endsection
